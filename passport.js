4d5e469bfb98571174539d05e713ffe9e93400b2
Jelloko <rainbow.samp@gmail.com>
2024-10-15 01:28:44 -0400

    Update passport.js

--- passport.js
+++ passport.js
@@ -6,7 +6,7 @@ const passport = require('passport'),
 let Users = Models.users,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
-
+//passport
 passport.use(
   new LocalStrategy(
     {
@@ -40,7 +40,7 @@ passport.use(
   )
 );
 
-//passport
+
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
   secretOrKey: 'your_jwt_secret'

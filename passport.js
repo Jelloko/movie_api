9d52d5eaf2b9570d3ee38fc536a5ddf8d1fc61a0
Jelloko <rainbow.samp@gmail.com>
2024-10-15 01:22:19 -0400

    Update passport.js

--- passport.js
+++ passport.js
@@ -10,7 +10,7 @@ let Users = Models.users,
 passport.use(
   new LocalStrategy(
     {
-      usernameField: 'Name',
+      NameField: 'Name',
       passwordField: 'Password',
     },
     async (Name, password, callback) => {

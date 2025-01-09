2b5e83a416615e7fa19b28834c5e7c033af5ff34
Jelloko <rainbow.samp@gmail.com>
2025-01-08 20:24:50 -0500

    Added Block Comments for JSDoc Documentation

--- auth.js
+++ auth.js
@@ -1,21 +1,45 @@
-const jwtSecret = 'your_jwt_secret'; 
+/**
+ * @constant {string} jwtSecret - Secret key used for signing JWT tokens.
+ */
+const jwtSecret = 'your_jwt_secret';
 
 const jwt = require('jsonwebtoken'),
   passport = require('passport');
 
-require('./passport'); 
+require('./passport'); // Passport configuration file
 
-
+/**
+ * Generates a JWT token for the provided user.
+ * 
+ * @param {Object} user - The user object to be encoded in the token.
+ * @param {string} user.Name - The name of the user, used as the token subject.
+ * @returns {string} The generated JWT token.
+ */
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
-    subject: user.Name, 
-    expiresIn: '7d', 
-    algorithm: 'HS256' 
+    subject: user.Name, // Subject of the token
+    expiresIn: '7d', // Token expiration time
+    algorithm: 'HS256' // Algorithm used to sign the token
   });
-}
+};
 
-//Login 
+/**
+ * Configures the login endpoint.
+ * 
+ * @param {Object} router - The Express router instance.
+ * @async
+ */
 module.exports = (router) => {
+  /**
+   * POST login endpoint.
+   * 
+   * @name POST /login
+   * @function
+   * @param {Object} req - Express request object.
+   * @param {Object} req.body - The body of the request, containing user credentials.
+   * @param {Object} res - Express response object.
+   * @returns {Object} JSON object containing the user and the JWT token.
+   */
   router.post('/login', (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
@@ -24,6 +48,7 @@ module.exports = (router) => {
           user: user
         });
       }
+
       req.login(user, { session: false }, (error) => {
         if (error) {
           res.send(error);
@@ -33,4 +58,4 @@ module.exports = (router) => {
       });
     })(req, res);
   });
-}
+};
